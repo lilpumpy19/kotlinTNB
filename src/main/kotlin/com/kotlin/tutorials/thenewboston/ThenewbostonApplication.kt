445bd82259484c30d0b3c3d2d6cb4b9beb963b3f
lilpumpy19 <100938542+lilpumpy19@users.noreply.github.com>
2023-10-01 15:44:57 +0400

    first commit

---
+++ src/main/kotlin/com/kotlin/tutorials/thenewboston/ThenewbostonApplication.kt
@@ -0,0 +1,11 @@
+package com.kotlin.tutorials.thenewboston
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ThenewbostonApplication
+
+fun main(args: Array<String>) {
+	runApplication<ThenewbostonApplication>(*args)
+}
---
+++ src/test/kotlin/com/kotlin/tutorials/thenewboston/ThenewbostonApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlin.tutorials.thenewboston
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ThenewbostonApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
